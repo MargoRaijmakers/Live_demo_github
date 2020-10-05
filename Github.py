b7a994b8401b555f7baf8151b6912ff483d91463
MargoRaijmakers <margoraijmakers@gmail.com>
2020-10-05 10:25:05 +0200

    git commit

---
+++ Github.py
@@ -0,0 +1,10 @@
+# file, settings, version control, github, +
+# maak nieuwe repository: + bij foto, new repository
+
+# terminal in pycharm
+#git init --> initialise .git
+#git add [file location/filename]
+#git commit
+#git remote add [naam] [url repository]
+#git push [name]
+
